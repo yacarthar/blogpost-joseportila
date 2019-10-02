a520694bc5739209c1eeed15ff7619fa09a3a126
hoangph <hoang.pham.vbhp@gmail.com>
2019-10-02 14:11:30 +0700

    fix celery part1

--- crawl_data/tasks.py
+++ crawl_data/tasks.py
@@ -11,53 +11,60 @@ mydb = myclient["zed"]
 mycol = mydb["post"]
 
 
-app1 = Celery('Post_queue',broker='redis://localhost:6379/0')
-app2 = Celery('IO_queue',broker='redis://localhost:6379/0')
-app1.config_from_object('config')
+app = Celery('Post_queue',broker='redis://localhost:6379/0')
+app.config_from_object('config')
 
-@app1.task
-def parsePost(data):
-    post_url = data['post_url']
-    # print(post_url)
-    post_id = post_url[post_url.rfind('-')+1:]
-    # print(post_id)
-    # id, title, url, path, content, time
+
+@app.task
+def handle_topic(topic):
     domain = 'https://quantrimang.com'
-    r = requests.get(domain + post_url)
+    while True:
+        r = requests.get(domain + topic)
+        soup = BeautifulSoup(r.text, 'lxml')
+        listviews = soup.find_all('div', {'class':'listview clearfix'})
+        for listview in listviews:
+            post = {}
+            for item in listview.ul.find_all('li'):
+                post['url'] = item.a.get('href')
+                post['desc'] = repr(item.div.get_text())
+                # send task
+                handle_post.delay(post)
+                print('push: ', post['url'].partition('-')[-1])
+
+        # next page
+        try:
+            viewmore = soup.find('a', {'class':'viewmore'})
+            topic = viewmore.get('href')
+        except:
+            break
+
+@app.task
+def handle_post(post):
+    url = post['url']
+    pid = url[url.rfind('-')+1:]
+    # pid, title, url, path, desc, content, time
+    domain = 'https://quantrimang.com'
+    r = requests.get(domain + url)
     soup = BeautifulSoup(r.text, 'lxml')
     # print(soup.prettify())
-    post_title = soup.find('div', {'id': 'contentMain'}).div.h1.get_text().strip()
-    # print(post_title)
-    post_path = soup.find('div', {'class': 'breadcrumbs info-detail'}).text.strip().replace('   ', '/')
-    # print(post_path)
-    post_content = '\n'.join([item.get_text() for item in soup.find('div', {'class': 'content-detail textview'}).find_all('p')])
-    # print(post_content[:15])
+    title = soup.find('div', {'id': 'contentMain'}).div.h1.get_text().strip()
+    path = soup.find('div', {'class': 'breadcrumbs info-detail'}).text.strip().replace('   ', '/')
+    content = '\n'.join([item.get_text() for item in soup.find('div', {'class': 'content-detail textview'}).find_all('p')])
     time = soup.find('div', {'class': 'author-info clearfix'}).get_text().strip().partition(', ')[2]
-    # print(time)
+    print(pid)
+    print(url)
+    print(title)
+    print(path)
+    print(content[:10])
+    print(time)
 
-    post = {
-        'post_id': post_id,
-        'post_title': post_title,
-        'post_url': post_url,
-        'post_path': post_path,
-        'post_content': post_content,
-        'post_time': time
-    }
-    # print(mydb.list_collection_names())
-    mycol.insert_one(post)
+    # post['pid']: pid
+    # post['title']: title
+    # post['path']: path
+    # post['content']: content
+    # post['time']: time
+    # mycol.insert_one(post)
 
 
 
-@app2.task
-def handleIO(post):
-    if 'stop' in post:
-        print('end')
-        #     # print([item['post_id'] for item in array])
-        #     # x = mycol.insert_many(array)
-        #     break
-        # array.append(post)
-        # if len(array) == 5000:
-        #     # print([item['post_id'] for item in array])
-        #     # x = mycol.insert_many(array)
-        #     print('db inserted')
-        #     array = []
+
