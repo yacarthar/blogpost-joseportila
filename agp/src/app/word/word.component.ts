da4253528f921ba50f88d8e49302b2d9853dbe7f
vernja_acer <hoang.pham.vbhp@gmail.com>
2019-10-16 09:10:42 +0700

    angular init

---
+++ agp/src/app/word/word.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: './word.component.html',
+    selector: 'app-word',
+    styleUrls: ['./word.component.css']
+})
+export class WordComponent {
+    en: string  = 'hello' ;
+    vn: string = 'Xin chao';
+    name = ''
+    urls = 'https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-600w-789676552.jpg'
+    forgot = false;
+    toggleForgot() {
+    	this.forgot = !this.forgot
+    };
+    showEvent(event) {
+    	console.log('UP')
+    	this.name = event.target.value
+    }
+
+}
