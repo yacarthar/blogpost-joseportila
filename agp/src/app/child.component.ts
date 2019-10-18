5c829b3257228ae494225140adb2da2af74a4a0c
vernja_acer <hoang.pham.vbhp@gmail.com>
2019-10-18 12:02:33 +0700

    View Child

--- agp/src/app/child.component.ts
+++ agp/src/app/child.component.ts
@@ -1,20 +1,13 @@
-import {Component, Output, EventEmitter} from '@angular/core';
-// import { EventEmitter } from 'events';
+import {Component} from '@angular/core';
+
 
 @Component(
     {
         selector: 'app-child',
-        template: `<h3><button (click)="addForParent()">Add</button> </h3>
-        <h3><button (click)="subForParent()">Sub</button> </h3>`
+        template: `<h3>{{ value }}</h3>`
     }
 )
 
 export class ChildComponent {
-    @Output() myClick = new EventEmitter<boolean>();
-    addForParent() {
-        this.myClick.emit(true);
-    }
-    subForParent() {
-        this.myClick.emit(false);
-    }
+    value = 0;
 }
--- agp/src/app/parent.component.ts
+++ agp/src/app/parent.component.ts
@@ -1,20 +1,17 @@
-import {Component} from '@angular/core';
-
+import {Component, ViewChild} from '@angular/core';
+import { ChildComponent } from './child.component';
 @Component(
     {
         selector: 'app-parent',
-        template: `<h3> {{ value }}</h3>
-        <app-child (myClick)="changeValue($event)"></app-child>`
+        template: `<button (click)="onAddForChild();">Add for child</button>
+        <app-child></app-child>`
     }
 )
 
 export class ParentComponent {
-    value = 0;
-    changeValue(isAdd: boolean) {
-        if (isAdd){
-            this.value = this.value + 1;
-        } else {
-            this.value = this.value - 1;
-        }
+    @ViewChild(ChildComponent)
+    myChild: ChildComponent;
+    onAddForChild() {
+        this.myChild.value++;
     }
 }
